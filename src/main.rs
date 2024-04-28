c53d3fac95131374437ab2e7b0c3f9bd871acd6c
brainstorm <brainstorm@nopcode.org>
2024-04-28 14:02:45 +1000

    Rewrite AVR I2C example into ESP32S3 (lolin 1.0.0 dev board)... getting a bootloop now :/
       Compiling embassy-avr-as5600-encoder v0.1.0 (/home/rvalls/dev/personal/embassy-avr-as5600-encoder)
        Finished release [optimized + debuginfo] target(s) in 54.48s
         Running `espflash flash --monitor target/xtensa-esp32s3-none-elf/release/embassy-avr-as5600-encoder`
    [2024-04-28T04:00:48Z INFO ] Serial port: '/dev/ttyUSB0'
    [2024-04-28T04:00:48Z INFO ] Connecting...
    [2024-04-28T04:00:49Z INFO ] Using flash stub
    Chip type:         esp32s3 (revision v0.1)
    Crystal frequency: 40 MHz
    Flash size:        16MB
    Features:          WiFi, BLE
    MAC address:       68:b6:b3:28:6c:bc
    App/part. size:    64/1,048,576 bytes, 0.01%
    [2024-04-28T04:00:51Z INFO ] Segment at address '0x0' has not changed, skipping write
    [2024-04-28T04:00:51Z INFO ] Segment at address '0x8000' has not changed, skipping write
    [2024-04-28T04:00:51Z INFO ] Segment at address '0x10000' has not changed, skipping write
    [2024-04-28T04:00:51Z INFO ] Flashing has completed!
    Commands:
        CTRL+R    Reset chip
        CTRL+C    Exit
    
    ESP-ROM:esp32s3-20210327
    Build:Mar 27 2021
    rst:0x1 (POWERON),boot:0x8 (SPI_FAST_FLASH_BOOT)
    SPIWP:0xee
    mode:DIO, clock div:2
    load:0x3fce3818,len:0x16f8
    load:0x403c9700,len:0x4
    load:0x403c9704,len:0xc00
    load:0x403cc700,len:0x2eb0
    entry 0x403c9908
    I (30) boot: ESP-IDF v5.1-beta1-378-gea5e0ff298-dirt 2nd stage bootloader
    I (30) boot: compile time Jun  7 2023 08:07:32
    I (31) boot: Multicore bootloader
    I (35) boot: chip revision: v0.1
    I (39) boot.esp32s3: Boot SPI Speed : 40MHz
    I (44) boot.esp32s3: SPI Mode       : DIO
    I (49) boot.esp32s3: SPI Flash Size : 4MB
    I (53) boot: Enabling RNG early entropy source...
    I (59) boot: Partition Table:
    I (62) boot: ## Label            Usage          Type ST Offset   Length
    I (70) boot:  0 nvs              WiFi data        01 02 00009000 00006000
    I (77) boot:  1 phy_init         RF data          01 01 0000f000 00001000
    I (85) boot:  2 factory          factory app      00 00 00010000 00100000
    I (92) boot: End of partition table
    I (96) boot: Loaded app from partition at offset 0x10000
    I (102) boot: Disabling RNG early entropy source...
    abort() was called at PC 0x403ceab9
    ESP-ROM:esp32s3-20210327
    Build:Mar 27 2021
    rst:0x10 (RTCWDT_RTC_RST),boot:0x8 (SPI_FAST_FLASH_BOOT)
    SPIWP:0xee
    mode:DIO, clock div:2
    load:0x3fce3818,len:0x16f8
    load:0x403c9700,len:0x4
    load:0x403c9704,len:0xc00
    load:0x403cc700,len:0x2eb0
    entry 0x403c9908
    I (30) boot: ESP-IDF v5.1-beta1-378-gea5e0ff298-dirt 2nd stage bootloader
    I (30) boot: compile time Jun  7 2023 08:07:32
    I (31) boot: Multicore bootloader
    I (35) boot: chip revision: v0.1
    I (39) boot.esp32s3: Boot SPI Speed : 40MHz
    I (44) boot.esp32s3: SPI Mode       : DIO
    I (49) boot.esp32s3: SPI Flash Size : 4MB
    I (53) boot: Enabling RNG early entropy source...
    I (59) boot: Partition Table:
    I (62) boot: ## Label            Usage          Type ST Offset   Length
    I (70) boot:  0 nvs              WiFi data        01 02 00009000 00006000
    I (77) boot:  1 phy_init         RF data          01 01 0000f000 00001000
    I (85) boot:  2 factory          factory app      00 00 00010000 00100000
    I (92) boot: End of partition table
    I (96) boot: Loaded app from partition at offset 0x10000
    I (102) boot: Disabling RNG early entropy source...
    abort() was called at PC 0x403ceab9

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,44 @@
 #![no_std]
 #![no_main]
-#![feature(impl_trait_in_assoc_type)]
 
-//use panic_halt as _;
-
-use arduino_hal::{ pins, prelude::_unwrap_infallible_UnwrapInfallible, I2c, Peripherals };
 use as5600::As5600;
 
 use embassy_executor::Spawner;
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl,
+    embassy::{self},
+    gpio::Io,
+    i2c::I2C,
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
+    timer::TimerGroup,
+};
 
-#[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    // disable interrupts - firmware has panicked so no ISRs should continue running
-    avr_device::interrupt::disable();
-
-    // get the peripherals so we can access serial and the LED.
-    //
-    // SAFETY: Because main() already has references to the peripherals this is an unsafe
-    // operation - but because no other code can run after the panic handler was called,
-    // we know it is okay.
-    let dp = unsafe { arduino_hal::Peripherals::steal() };
-    let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
-
-    // Print out panic location
-    ufmt::uwriteln!(&mut serial, "Firmware panic!\r").unwrap_infallible();
-    if let Some(loc) = info.location() {
-        ufmt::uwriteln!(
-            &mut serial,
-            "  At {}:{}:{}\r",
-            loc.file(),
-            loc.line(),
-            loc.column(),
-        )
-        .unwrap_infallible();
-    }
-
-    // Blink LED rapidly
-    let mut led = pins.d13.into_output();
-    loop {
-        led.toggle();
-        arduino_hal::delay_ms(100);
-    }
-}
-
-#[embassy_executor::main]
+#[main]
 async fn main(_spawner: Spawner) {
-    let peripherals = Peripherals::take().unwrap();
-    let pins = pins!(peripherals);
-    let mut serial = arduino_hal::default_serial!(peripherals, pins, 115200);                                                                        
-    let i2c = I2c::new(
-        peripherals.TWI,
-        pins.d20.into_pull_up_input(),
-        pins.d21.into_pull_up_input(),
-        50000,
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timg0);
+
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let i2c = I2C::new_async(
+        peripherals.I2C0,
+        io.pins.gpio21,
+        io.pins.gpio14,
+        50.kHz(),
+        &clocks,
     );
 
     let mut encoder = As5600::new(i2c);
 
     loop {
-        ufmt::uwriteln!(&mut serial, "{}", "Reading angle").unwrap();
-        let status = encoder.angle().unwrap();
-        ufmt::uwriteln!(&mut serial, "{}", "Angle has been read").unwrap_infallible();
-        ufmt::uwriteln!(&mut serial, "{}", status).unwrap_infallible();
+        let status = encoder.angle();
+        esp_println::println!("{:?}", status);
     }
 }
