1f7a12d6829932b28b37823552824912607aba92
brainstorm <brainstorm@nopcode.org>
2024-06-25 20:02:17 +1000

    Magnet readings work on esp32s3

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,44 @@
 #![no_std]
 #![no_main]
-#![feature(type_alias_impl_trait)]
+
+use as5600::As5600;
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
-    embassy::{self},
-    peripherals::Peripherals,
-    prelude::*,
-    system::SystemControl,
-    timer::TimerGroup,
+    clock::ClockControl, delay::Delay, embassy, gpio::Io, i2c::I2C, peripherals::Peripherals, prelude::*, system::SystemControl, timer::TimerGroup
 };
 
-#[embassy_executor::task]
-async fn run() {
-    loop {
-        esp_println::println!("Hello world!");
-        Timer::after(Duration::from_millis(1_000)).await;
-    }
-}
 
 #[main]
-async fn main(spawner: Spawner) {
-    esp_println::println!("Init!");
+async fn main(_spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
-
-    spawner.spawn(run()).ok();
-
+    esp_println::println!("Bing!");
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    
+    let i2c = I2C::new_async(
+        peripherals.I2C0,
+        io.pins.gpio41,
+        io.pins.gpio42,
+        400.kHz(),
+        &clocks,
+    );
+    let delay = Delay::new(&clocks);
+    
+    let mut encoder = As5600::new(i2c);
     loop {
-        esp_println::println!("Bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
+        if let Ok(angle) = encoder.raw_angle() {
+            esp_println::println!("{:?}", angle);
+            delay.delay_millis(300);
+        };
+        if let Ok(magnet) = encoder.magnet_status() {
+            esp_println::println!("{:?}", magnet);
+            delay.delay_millis(300);
+        };
     }
 }
-
-
-// #![no_std]
-// #![no_main]
-// #![feature(type_alias_impl_trait)]
-
-// use as5600::As5600;
-
-// use embassy_executor::Spawner;
-// use esp_backtrace as _;
-// use esp_hal::{
-//     clock::ClockControl,
-//     embassy::{self},
-//     gpio::Io,
-//     i2c::I2C,
-//     peripherals::Peripherals,
-//     prelude::*,
-//     system::SystemControl,
-//     timer::TimerGroup,
-// };
-
-// #[main]
-// async fn main(_spawner: Spawner) {
-//     let peripherals = Peripherals::take();
-//     let system = SystemControl::new(peripherals.SYSTEM);
-//     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-//     let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
-//     embassy::init(&clocks, timg0);
-//     esp_println::println!("Bing!");
-//     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    
-//     let i2c = I2C::new_async(
-//         peripherals.I2C0,
-//         io.pins.gpio21,
-//         io.pins.gpio14,
-//         100.kHz(),
-//         &clocks,
-//     );
-
-//     let mut encoder = As5600::new(i2c);
-
-//     loop {
-//         let status = encoder.angle();
-//         esp_println::println!("{:?}", status);
-//     }
-// }
